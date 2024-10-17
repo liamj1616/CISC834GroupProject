14c2ffab2bae965e449314e79e1a73411131ea17
genglin6688 <genglin666888@gmail.com>
2024-10-17 17:40:39 -0400

    made method to get all model names and save them in file

--- Models.py
+++ Models.py
@@ -6,6 +6,8 @@ from dateutil import parser
 import os
 from huggingface_hub.utils import disable_progress_bars
 import random
+import time
+import json
 
 
 class Models:
@@ -18,7 +20,7 @@ class Models:
         init method
         """
         self.api = HfApi()
-        self.models = None
+        self.models = []
         self.models_filtered = []
         warnings.filterwarnings("ignore", message="Invalid model-index")
         warnings.filterwarnings("ignore", message=".*HF_HUB_DISABLE_SYMLINKS_WARNING.*")
@@ -29,8 +31,31 @@ class Models:
         pulls the models, with specified limit amount of models
         full=True makes filter by date possible
         """
-        self.models = list(self.api.list_models(sort="downloads", direction=-1, limit=limit, full=full))
+        self.models = list(self.api.list_models(sort="createdAt", direction=-1, full=full))
         print("fetched %s models" % len(self.models))
+
+    def write_models(self):
+        models = []
+        for model in self.models:
+            models.append([model.modelId, model.createdAt])
+        self.models = models
+        with open('model_names.json', 'w') as file:
+            for model in models:
+                file.write(json.dumps(model) + '\n')
+
+    # def get_models_time(self, limit=10000, full=True):
+    #     start_date = datetime(2022, 9, 1, tzinfo=timezone.utc)
+    #     end_date = datetime(2024, 8, 31, tzinfo=timezone.utc)
+    #     i = 0
+    #     while len(self.models) < limit:
+    #         i += 1
+    #         models = list(self.api.list_models(sort="createdAt", page, direction=-1, full=full, pagesize=100))
+    #         print(len(models))
+    #         print(parser.isoparse(str(models[0].createdAt)))
+    #         if start_date <= parser.isoparse(str(models[0].createdAt)) <= end_date:
+    #             for model in models:
+    #                 self.models.append(model)
+    #     print("fetched %s models" % len(self.models))
 
     def filter_date(self):
         """
@@ -61,7 +86,7 @@ class Models:
         filter the models with empty datacard out
         """
         with ThreadPoolExecutor(max_workers=100) as executor:
-            percents = 100
+            percents = 10
             for i in range(0, percents):
                 models = self.models[
                          round(len(self.models) * i / percents):round(len(self.models * (i + 1)) / percents)]
@@ -79,6 +104,7 @@ class Models:
                     print("Hugging Face cache has been cleared.")
                 else:
                     print("Hugging Face cache directory does not exist.")
+                time.sleep(3)
 
         # self.models = [model for model, result in zip(self.models, results) if result is not None]
         print("%s models have data cards" % len(self.models_filtered))
@@ -93,7 +119,8 @@ class Models:
 
 if __name__ == "__main__":
     md = Models()
-    md.get_models(limit=10000, full=True)
-    md.filter_date()
-    md.filter_empty()
-    md.select_400()
+    md.get_models(limit=1000, full=True)
+    md.write_models()
+    # md.filter_date()
+    # md.get_models_time(limit=55300)
+    # md.filter_empty()
